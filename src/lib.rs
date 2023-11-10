c071842991858f1079f7a6eb9218a12a7e7f3b0d
massivebird <gdrakemail@gmail.com>
2023-11-09 19:41:27 -0500

    feat: functional stitch function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/// # Panics
+///
+/// Will panic if the two string slices do not have the same number of lines
+pub fn stitch<'a>(left: &'a str, right: &'a str) -> String {
+    assert_eq!(left.lines().count(), right.lines().count());
+
+    let num_lines = right.lines().count();
+
+    let my_iterator = |s: &'a str, n: usize| {
+        s.lines().nth(n).unwrap()
+    };
+
+    let mut result = String::new();
+
+    result.push_str(my_iterator(left, 0));
+    result.push_str(my_iterator(right, 0));
+
+    for n in 1..num_lines {
+        result.push('\n');
+        result.push_str(my_iterator(left, n));
+        result.push_str(my_iterator(right, n));
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -7,8 +29,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn stitch_1() {
+        let left  = "L\nL";
+        let right = "R\nR";
+        assert_eq!(stitch(left, right), "LR\nLR");
+    }
+
+    #[test]
+    fn stitch_2() {
+        let left  = "AB\nEF";
+        let right = "CD\nGH";
+        assert_eq!(stitch(left, right), "ABCD\nEFGH");
     }
 }
